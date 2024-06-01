364d688c8180a13d6d25659ab77da055768ee63c
Raissa Reis Lopes <raissa.vilemon@gmail.com>
2024-06-01 15:21:38 -0300

    feat: add create team repository

--- src/repositories/teamRepository.ts
+++ src/repositories/teamRepository.ts
@@ -1,6 +1,18 @@
 import { pool } from "../database/connection";
 import { ITeam } from "../interfaces/team";
 
+export const createTeam = async (team: ITeam): Promise<ITeam> => {
+  const query = "INSERT INTO teams (name, leader) VALUES ($1, $2) RETURNING *";
+  const values = [team.name, team.leaderId];
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0] as ITeam;
+  } catch (error: any) {
+    console.log("Falha ao criar uma nova equipe", error);
+    throw new Error("Falha ao criar uma nova equipe");
+  }
+};
+
 export const getTeamByLeaderId = async (
   leaderId: string,
 ): Promise<ITeam | null> => {
---
+++ src/services/teamServices.ts
@@ -0,0 +1,6 @@
+import * as teamRepository from "../repositories/teamRepository";
+import { IUser } from "../interfaces/user";
+import { isAdmin } from "../utils/isAdmin";
+import { isLeader } from "../utils/isLeader";
+
+
